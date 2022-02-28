76e7acdd76787f75bf4bc85e58661f7a571183a5
Mohammad Mobin Ghoveoud <mobin.sgh@gmail.com>
2022-03-01 00:39:04 +0330

    Add session
    Save page number in session

--- tasks/views.py
+++ tasks/views.py
@@ -2,13 +2,19 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from tasks.models import Task
+from django.http import HttpResponse
 
 
 def index(request):
-    paginator = Paginator(Task.objects.all(), 2)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    paginator = Paginator(Task.objects.all(), 1)
 
+    page_number = request.GET.get('page')
+    if request.GET.get('page') == None:
+        page_number = request.session.get('page_number', default=1)
+    request.session['page_number'] = page_number
+
+    page_obj = paginator.get_page(page_number)
+    # return HttpResponse(request.session.get('page_number'))
     return render(request, 'tasks/index.html', {'page_obj': page_obj})
 
 
